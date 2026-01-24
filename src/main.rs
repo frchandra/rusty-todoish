2f99d621668e03135917544f80efc60d351e0008
frchandra <nismara.chandra@gmail.com>
2026-01-24 23:44:34 +0900

    testing postgres database connectivity

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
 mod api;
 
+use dotenvy::dotenv;
+use sqlx::PgPool;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = api::routes::health_check_routes::routes();
+    dotenv().ok();
+    println!("Starting server...");
+
+    let database_url = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
+        let db = std::env::var("POSTGRES_DATABASE_NAME").unwrap();
+        let user = std::env::var("POSTGRES_USERNAME").unwrap();
+        let pass = std::env::var("POSTGRES_PASSWORD").unwrap();
+        let host = std::env::var("POSTGRES_ADDRESS").unwrap();
+        let port = std::env::var("POSTGRES_PORT").unwrap();
+
+        format!("postgres://{}:{}@{}:{}/{}", user, pass, host, port, db)
+    });
+
+    let pool = match PgPool::connect(&database_url).await {
+        Ok(pool) => {
+            println!("Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            eprintln!("Failed to connect to the database: {err}");
+            std::process::exit(1);
+        }
+    };
+
+    match sqlx::query("SELECT 1").execute(&pool).await {
+        Ok(_) => {
+            println!("Database ping successful");
+        }
+        Err(err) => {
+            eprintln!("Database ping failed: {err}");
+            std::process::exit(1);
+        }
+    }
+
+
     //create router from the router module
+    let app = api::routes::health_check_routes::routes();
+
 
     println!("Server started successfully at 0.0.0.0:8080");
 
