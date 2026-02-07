c1b7cf60cd70d40add2fd9d1fab93be9cdc07001
frchandra <nismara.chandra@gmail.com>
2026-02-07 13:01:26 +0900

    adding test case for list notes

--- tests/health_check_test.rs
+++ tests/health_check_test.rs
@@ -16,7 +16,18 @@ async fn health_check_test() {
 
     let body = response.text().await.unwrap();
     let json: serde_json::Value = serde_json::from_str(&body).unwrap();
-    println!("AAAA {:#?}", json["message"]);
+    println!("Message log : {:#?}", json["message"]);
 
     assert_eq!(json["message"], "/rest/v1");
+}
+
+#[tokio::test]
+#[serial]
+async fn list_notes_test() {
+    // Start rest server
+    rest_app_test::start_server().await;
+
+    let url = "http://127.0.0.1:8080/api/v1/notes";
+    let response = reqwest::get(url).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
 }
