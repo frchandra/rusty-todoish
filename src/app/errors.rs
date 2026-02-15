b311dc2c7fedb27e84bf8b72e053ea35a05c601c
frchandra <nismara.chandra@gmail.com>
2026-02-15 10:55:02 +0900

    modifying error handling

--- src/app/errors.rs
+++ src/app/errors.rs
@@ -1,16 +1,39 @@
-use std::fmt::{Display, Formatter, Result};
-
-use axum::{
-    Json,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter, Result};
 
 pub struct AppError {
     pub error_code: AppErrorCode,
-    pub errors: Vec<ErrorEntry>,
+    pub error_details: Vec<ErrorEntry>,
+}
+
+impl AppError {
+    pub fn new(app_error_code: AppErrorCode, error_details: ErrorEntry) -> Self {
+        Self {
+            error_code: app_error_code, // Default code, can be overridden later.
+            error_details: vec![error_details],
+        }
+    }
+}
+impl From<sqlx::Error> for AppError {
+    fn from(e: sqlx::Error) -> Self {
+        // Do not disclose database-related internal specifics, except for debug builds.
+        if cfg!(debug_assertions) {
+            let error_code = match e {
+                sqlx::Error::RowNotFound => AppErrorCode::ResourceNotFound,
+                _ => AppErrorCode::InternalServerError,
+            };
+            Self::new(error_code, ErrorEntry::new(&e.to_string()))
+        } else {
+            // Build the entry with a trace id to find the exact error in the log when needed.
+            let error_code = AppErrorCode::DatabaseError;
+            let error_entry = ErrorEntry::new(&e.to_string()).trace_id();
+            let trace_id = error_entry.trace_id.as_deref().unwrap_or("");
+            // The error must be logged here. Otherwise, we would lose it.
+            tracing::error!("SQLx error: {}, trace id: {}", e.to_string(), trace_id);
+            Self::new(error_code, error_entry)
+        }
+    }
 }
 
 //implement Display for AppError
@@ -19,27 +42,15 @@ impl Display for AppError {
         write!(
             f,
             "AppError: {:?}, Errors: {:?}",
-            self.error_code, self.errors
+            self.error_code, self.error_details
         )
-    }
-}
-
-impl From<sqlx::Error> for AppError {
-    fn from(error: sqlx::Error) -> Self {
-        let status_code = match error {
-            sqlx::Error::RowNotFound => AppErrorCode::ResourceNotFound,
-            _ => AppErrorCode::DatabaseError,
-        };
-        Self {
-            error_code: status_code,
-            errors: vec![ErrorEntry::from(error)],
-        }
     }
 }
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum AppErrorCode {
+    InternalServerError,
     AuthenticationWrongCredentials,
     AuthenticationMissingCredentials,
     AuthenticationTokenCreationError,
@@ -72,8 +83,6 @@ impl Display for AppErrorCode {
 pub struct ErrorEntry {
     pub message: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub instance: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub trace_id: Option<String>,
     pub timestamp: DateTime<Utc>,
 }
@@ -87,11 +96,6 @@ impl ErrorEntry {
         }
     }
 
-    pub fn instance(mut self, instance: &str) -> Self {
-        self.instance = Some(instance.to_owned());
-        self
-    }
-
     pub fn trace_id(mut self) -> Self {
         // Generate a new trace id.
         let mut trace_id = uuid::Uuid::new_v4().to_string();
@@ -101,26 +105,26 @@ impl ErrorEntry {
     }
 }
 
-impl From<sqlx::Error> for ErrorEntry {
-    fn from(e: sqlx::Error) -> Self {
-        // Do not disclose database-related internal specifics, except for debug builds.
-        /*        if cfg!(debug_assertions) {
-            let (code, kind) = match e {
-                sqlx::Error::RowNotFound => (
-                    APIErrorCode::ResourceNotFound,
-                    APIErrorKind::ResourceNotFound,
-                ),
-                _ => (APIErrorCode::DatabaseError, APIErrorKind::DatabaseError),
-            };
-            Self::new(&e.to_string()).code(code).kind(kind).trace_id()
-        } else {
-            // Build the entry with a trace id to find the exact error in the log when needed.
-            let error_entry = Self::from(StatusCode::INTERNAL_SERVER_ERROR).trace_id();
-            let trace_id = error_entry.trace_id.as_deref().unwrap_or("");
-            // The error must be logged here. Otherwise, we would lose it.
-            tracing::error!("SQLx error: {}, trace id: {}", e.to_string(), trace_id);
-            error_entry
-        }*/
-        Self::new(&e.to_string()).trace_id()
-    }
-}
+// impl From<sqlx::Error> for ErrorEntry {
+//     fn from(e: sqlx::Error) -> Self {
+//         // Do not disclose database-related internal specifics, except for debug builds.
+//         /*        if cfg!(debug_assertions) {
+//             let (code, kind) = match e {
+//                 sqlx::Error::RowNotFound => (
+//                     APIErrorCode::ResourceNotFound,
+//                     APIErrorKind::ResourceNotFound,
+//                 ),
+//                 _ => (APIErrorCode::DatabaseError, APIErrorKind::DatabaseError),
+//             };
+//             Self::new(&e.to_string()).code(code).kind(kind).trace_id()
+//         } else {
+//             // Build the entry with a trace id to find the exact error in the log when needed.
+//             let error_entry = Self::from(StatusCode::INTERNAL_SERVER_ERROR).trace_id();
+//             let trace_id = error_entry.trace_id.as_deref().unwrap_or("");
+//             // The error must be logged here. Otherwise, we would lose it.
+//             tracing::error!("SQLx error: {}, trace id: {}", e.to_string(), trace_id);
+//             error_entry
+//         }*/
+//         Self::new(&e.to_string()).trace_id()
+//     }
+// }
