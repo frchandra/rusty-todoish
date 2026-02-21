58c58494ab98f3ceea0f691644eb69a980d791e5
frchandra <nismara.chandra@gmail.com>
2026-02-21 14:23:06 +0900

    updating note's DTO field
    Signed-off-by: frchandra <nismara.chandra@gmail.com>

--- src/models/note.rs
+++ src/models/note.rs
@@ -7,7 +7,7 @@ pub struct NoteModel {
     pub id: uuid::Uuid,
     pub title: String,
     pub content: String,
-    pub is_published: i8,
+    pub is_published: bool,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
